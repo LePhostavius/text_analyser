7c3296af30e36b5c38d9f1d302848d0fa1e50eb8
Louis Falce <louis.falce@hotmail.fr>
2022-05-05 10:30:39 +0200

    first commit

---
+++ bonjour.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+class bonjour:
+    
+        def bonjour():
+            print("bonjour")
---
+++ main.py
@@ -0,0 +1,8 @@
+from text_analyser.bonjour import bonjour
+
+class main:
+
+        t = bonjour
+        t.bonjour()
+        
+    
